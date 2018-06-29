98ae350156c595553f0570583d5950daa304eed0
Lydia Dick <lydiadick@gmail.com>
2018-06-29 03:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// 1. receives one argument and returns it in all caps
 function shout(string) {
   return string.toUpperCase()
 }
 
-//2. receives one argument and returns it in all lowercase
+// 2. receives one argument and returns it in all lowercase
+
