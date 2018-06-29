61380159c0a5383cfa798f7eb0235c299b1d95bb
Lydia Dick <lydiadick@gmail.com>
2018-06-29 04:51:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
-    var text ="I can't hear you!";
+    var text ="I can't hear you!"
   }
   else if (string.toUpperCase === string) {
-    text ="YES INDEED!";
+    text ="YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
-    text ="I love you, too.";
+    text ="I love you, too."
   }
   return text ;
 }
